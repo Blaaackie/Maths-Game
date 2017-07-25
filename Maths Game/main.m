948b42d5692902f9a49a11800ca74da9ea3228c6
Tye Blackie <blackie.tye@gmail.com>
2017-07-25 13:39:28 -0700

    Initial Commit

---
+++ Maths Game/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths Game
+//
+//  Created by Tye Blackie on 2017-07-25.
+//  Copyright © 2017 Tye Blackie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
